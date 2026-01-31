2b380ae210864e0cbc9f243472c7cc6279e6b0c8
echo-search <hassaanulhoda@gmail.com>
2026-01-31 22:32:34 +0000

    Create VoiceSearchActivity.kt

---
+++ essentials/app/src/main/java/com/echosearch/package/VoiceSearchActivity.kt
@@ -0,0 +1,33 @@
+class VoiceSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            putExtra(RecognizerIntent.EXTRA_PROMPT, "Search EchoSearch")
+        }
+
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            val query = data
+                ?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                ?.firstOrNull()
+
+            if (query != null) {
+                val searchIntent = Intent(this, MainActivity::class.java)
+                searchIntent.putExtra("query", query)
+                startActivity(searchIntent)
+            }
+        }
+        finish()
+    }
+}
